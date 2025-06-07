f4bf30099da8037d78d7bc78ab32d6d886f542d9
xxbaizero <xxbaizero@gmail.com>
2025-06-07 17:48:38 +0800

    sytles(materials): 删除一些没有用到的属性

--- src/editor/materials/Container/prod.tsx
+++ src/editor/materials/Container/prod.tsx
@@ -4,7 +4,7 @@ import { type CommonComponentProps } from "../../interface";
  * @description Container 组件的“生产”版本，用于预览和最终渲染。
  * @see /src/editor/materials/README.md - 详细规范请参考物料组件开发文档。
  */
-const ContainerProd = ({ id, children, styles }: CommonComponentProps) => {
+const ContainerProd = ({ children, styles }: CommonComponentProps) => {
   return (
     <div style={styles} className={`p-[20px]`}>
       {children}
--- src/editor/materials/Page/prod.tsx
+++ src/editor/materials/Page/prod.tsx
@@ -4,7 +4,7 @@ import { type CommonComponentProps } from "../../interface";
  * @description Page 组件的“生产”版本，用于预览和最终渲染。
  * @see /src/editor/materials/README.md - 详细规范请参考物料组件开发文档。
  */
-function PageProd({ id, name, children, styles }: CommonComponentProps) {
+function PageProd({ children, styles }: CommonComponentProps) {
   return (
     <div className="p-[20px]" style={{ ...styles }}>
       {children}
