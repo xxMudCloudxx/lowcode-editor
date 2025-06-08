7f5073bffbfbd5f7c9c8642337a5cadc21d8257e
xxbaizero <xxbaizero@gmail.com>
2025-06-08 22:14:21 +0800

    styles(Page): 刪除一些用不到的属性

--- src/editor/materials/Page/dev.tsx
+++ src/editor/materials/Page/dev.tsx
@@ -5,13 +5,7 @@ import { useMaterailDrop } from "../../hooks/useMatrialDrop";
  * @description Page 组件的“开发”版本，用于编辑器画布内。
  * @see /src/editor/materials/README.md - 详细规范请参考物料组件开发文档。
  */
-function PageDev({
-  children,
-  id,
-  styles,
-  name,
-  isSelected,
-}: CommonComponentProps) {
+function PageDev({ children, id, name, isSelected }: CommonComponentProps) {
   const { isOver, drop } = useMaterailDrop(id, name);
   return (
     <div
@@ -20,7 +14,7 @@ function PageDev({
         isSelected
           ? "" // 如果被选中，就不要任何边框和轮廓，完全交给 SelectedMask
           : `border border-solid border-[#000] ${
-              isOver ? "outline  outline-blue-500" : ""
+              isOver ? "outline outline-blue-500" : ""
             }` // 否则，显示常规边框和悬浮轮廓
       }`}
       data-component-id={id}
