5e3da8bc2ca6a3a508b310291097fed8d8a05121
xxbaizero <xxbaizero@gmail.com>
2025-06-14 02:14:38 +0800

    feat: 为GitHub Pages添加了Github Action

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const REPO_NAME = "lowcode-editor";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${REPO_NAME}/`,
   plugins: [react(), tailwindcss()],
 });
