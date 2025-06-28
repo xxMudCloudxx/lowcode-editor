db23147ecf77ba3405d171224ad526662964ce77
xxbaizero <xxbaizero@gmail.com>
2025-06-28 23:27:49 +0800

    fix(Monaco Editor)：修复了从CDN下载Monaco有时候因网络问题而失败的BUG
    解决方法：使用vite-plugin-monaco-editor-esm的插件将Monaco Editor直接作为项目的一部分，加载不再依赖外部网络

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import monacoEditorPlugin from "vite-plugin-monaco-editor-esm";
 
 const REPO_NAME = "lowcode-editor";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: `/${REPO_NAME}/`,
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    monacoEditorPlugin({
+      languageWorkers: ["json", "css", "html", "typescript"],
+    }),
+  ],
 });
