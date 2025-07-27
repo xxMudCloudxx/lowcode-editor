38b1a8956d818035488f8f94f16b7120795df167
xxMudCloudxx <xxbaizero@gmail.com>
2025-07-27 09:17:03 +0800

    docs：添加适当的注释作为项目结构解释

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import ReactPlayground from "./editor";
 
+// 主入口
 function App() {
   return <ReactPlayground />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+
+// 使用Dnd作为拖拽逻辑实现的方法
 createRoot(document.getElementById("root")!).render(
   <DndProvider backend={HTML5Backend}>
     <App />
